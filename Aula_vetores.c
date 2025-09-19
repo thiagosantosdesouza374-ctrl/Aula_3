7ef8bcc8e604765c2ffb45d94529487c938ca156
thiagosantosdesouza374-ctrl <thiagosantosdesouza374@gmail.com>
2025-09-18 21:09:37 -0300

    Add files via upload

--- Aula_vetores.c
+++ Aula_vetores.c
@@ -2,23 +2,32 @@
 #define TAM 4
 int main(){
     
-    int valores[4];
+    int valores[TAM];
+    int uso_memoria;
+    int tamanho_vetor;
+    
     /*valores[0]=4;
     valores[1]=7;
     valores[2]=3;
     valores[3]=1;*/
 
     printf("\nOla! O sistema vai pedir 4 valores.");
-    for(int i=0; i<4;i--){
+    for(int i=0; i<TAM;i++){
         printf("\nInsira um valor: ");
         scanf("%d", &valores[i]);
     }
-
     printf("Os valores armazenados sao: ");
-    for (int i=0; i<4;i--){
+    for (int i=0; i<TAM;i++){
         printf("%d", valores[i]);
     }
-    printf("\n%d",sizeof(&valores));
+     printf("Os valores armazenados sao: ");
+    for (int i=TAM; i>-1;i--){
+        printf("%d", valores[i]);
+    }
+    uso_memoria = sizeof(valores);
+    tamanho_vetor = sizeof(valores)/ sizeof(valores[0]);
+    printf("\n%d Tamanhode espacoem memoria em bytes",uso_memoria);
+    printf("\n%d Numero de elementos",tamanho_vetor);
     printf("\nFim do progama!");
 
     return 0;
